dd939c916c509bfa60edc73454ce32a30499aa08
jtran2509 <jadetran2509@gmail.com>
2023-05-06 20:34:27 -0400

    add app_setup file

---
+++ app_setup.py
@@ -0,0 +1,20 @@
+import os
+
+import streamlit as st
+
+
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+
+if __name__ == '__main__':
+    # Select a file
+    if st.checkbox('Select a file in current directory'):
+        folder_path = '.'
+        if st.checkbox('Change directory'):
+            folder_path = st.text_input('Enter folder path', '.')
+        filename = file_selector(folder_path=folder_path)
+        st.write('You selected `%s`' % filename)
+
