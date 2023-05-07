7ae744abfb356bdcff1be044fe071dded2a3e922
jtran2509 <jadetran2509@gmail.com>
2023-05-06 20:56:17 -0400

    make some changes

--- app_setup.py
+++ app_setup.py
@@ -6,6 +6,9 @@ import numpy as np
 model = pickle.load(open('export.pkl', 'rb')
 
 st.title("Pneumonia Chest Xray detection")
+
+
+# Heading
 html_temp = """
 <div style="background:#025246 ;padding:10px">
 <h2 style="color:white;text-align:center;"> Pneumonia ML App </h2>
@@ -13,21 +16,6 @@ html_temp = """
 """
 st.markdown(html_temp, unsafe_allow_html = True)
 
-safe_html ="""  
-<div style="background-color:#80ff80; padding:10px >
-<h2 style="color:white;text-align:center;"> The Abalone is young</h2>
-</div>
-"""
-if st.button("Predict the type"):
-output = predict_age(Length,Diameter,Height,Whole_weight, Shucked_weight,Viscera_weight,Shell_weight)
-st.success('The age is {}'.format(output))
-
-        if output == 1:
-            st.markdown(safe_html,unsafe_allow_html=True)
-        elif output == 2:
-            st.markdown(warn_html,unsafe_allow_html=True)
-        elif output == 3:
-            st.markdown(danger_html,unsafe_allow_html=True)
 
 
 def file_selector(folder_path='.'):
