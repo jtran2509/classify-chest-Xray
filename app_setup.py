e782dbe631190122a74efd763858e13b23887bf5
jtran2509 <jadetran2509@gmail.com>
2023-05-23 22:17:44 -0400

    change title and heading

--- app_setup.py
+++ app_setup.py
@@ -31,7 +31,5 @@ if __name__ == '__main__':
         filename = file_selector(folder_path=folder_path)
         st.write('You selected `%s`' % filename)
 
-learn_inf.predict('/content/drive/MyDrive/fastai/test/NORMAL/IM-0109-0001.jpeg')
-
 
 
